b6f4fd2d33353d1370f65c2508726e437dee8d37
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2014-12-17 17:11:07 -0800

    Test: Add Context tests.

--- test/context.js
+++ test/context.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
+var dispatcher = require('littlest-dispatcher');
 var Context = require('../lib/context');
-var littlestDispatcher = require('littlest-dispatcher');
+var Empty = require('./fixtures/components/empty');
 
 function noop() {}
 
@@ -50,7 +51,7 @@ describe('Context', function () {
 
       expect(child.stores).to.be.an('object');
       expect(child.stores).to.have.keys(['test']);
-      expect(child.stores.test).to.be.an.instanceof(littlestDispatcher.Store);
+      expect(child.stores.test).to.be.an.instanceof(dispatcher.Store);
     });
 
     it('should start with the same Store state', function () {
@@ -98,6 +99,167 @@ describe('Context', function () {
           expect(child.stores.test).to.have.property('foo', 42);
         })
         .then(done, done);
+    });
+  });
+
+  describe('getStore', function () {
+    it('should return a Store');
+    it('should return null if missing');
+  });
+
+  describe('performAction', function () {
+    it('should return a Promise');
+    it('should perform an Action');
+    it('should throw if missing');
+  });
+
+  describe('createRoute', function () {
+    beforeEach(function () {
+      this.context.createRoute('test', {
+        path: '/test',
+        body: Empty
+      });
+    });
+
+    it('should make routes available through router.getRoute', function () {
+      var route = this.context.router.getRoute('/test');
+
+      expect(route).to.be.an('object');
+      expect(route).to.have.property('body', Empty);
+    });
+
+    it('should make routes available through getRouteUrl', function () {
+      var url = this.context.getRouteUrl('test');
+
+      expect(url).to.equal('/test');
+    });
+  });
+
+  describe('createErrorRoute', function () {
+    beforeEach(function () {
+      this.context.createErrorRoute(400, {
+        body: Empty
+      });
+    });
+
+    it('should make routes available through router.getErrorRoute', function () {
+      var route = this.context.router.getErrorRoute(400);
+
+      expect(route).to.be.an('object');
+      expect(route).to.have.property('body', Empty);
+    });
+
+    it('should support error names', function () {
+      this.context.createErrorRoute('NotFound', {
+        body: Empty
+      });
+
+      var route = this.context.router.getErrorRoute(404);
+
+      expect(route).to.be.an('object');
+      expect(route).to.have.property('body', Empty);
+    });
+  });
+
+  describe('getRouteUrl', function () {
+    beforeEach(function () {
+      this.context.createRoute('test', {
+        path: '/test',
+        body: Empty
+      });
+
+      this.context.createRoute('test:view', {
+        path: '/test/:testId',
+        body: Empty
+      });
+    });
+
+    it('should render parameterless URLs', function () {
+      expect(this.context.getRouteUrl('test')).to.equal('/test');
+    });
+
+    it('should render parameterful URLs', function () {
+      expect(this.context.getRouteUrl('test:view', { testId: 'foo' })).to.equal('/test/foo');
+    });
+
+    it('should return null for missing Routes', function () {
+      var route = this.context.getRouteUrl('missing');
+
+      expect(route).to.be.null();
+    });
+  });
+
+  describe('JSON', function () {
+    beforeEach(function () {
+      this.context.createStore('test')
+        .define('foo', 'bar');
+
+      this.context.createAction('test:go', noop);
+      this.json = this.context.toJSON();
+    });
+
+    it('should be valid', function () {
+      var self = this;
+
+      expect(function () {
+        JSON.parse(JSON.stringify(self.json));
+      }).to.not.throw();
+    });
+
+    it('should be an Object', function () {
+      expect(this.json).to.be.an('object');
+    });
+
+    it('should contain Store state', function () {
+      expect(this.json.stores).to.be.an('object');
+    });
+
+    it('should not contain Actions', function () {
+      expect(this.json.actions).to.not.exist();
+    });
+  });
+
+  describe('fromObject', function () {
+    beforeEach(function () {
+      this.context.createStore('test')
+        .define('foo', 'bar');
+
+      this.context.createAction('test:go', noop);
+    });
+
+    it('should restore Store state', function () {
+      var context = new Context();
+
+      context.createStore('test');
+      context.fromObject(JSON.parse(JSON.stringify(this.context)));
+
+      expect('foo' in context.getStore('test')).to.be.true();
+      expect(context.getStore('test').foo).to.equal('bar');
+    });
+
+    it('should not restore Actions', function () {
+      var context = new Context();
+
+      context.fromObject(JSON.parse(JSON.stringify(this.context)));
+
+      return context.performAction('test:go')
+        .then(function () {
+          throw new Error('Expected performAction to throw');
+        }, function () {
+          return null;
+        });
+    });
+
+    it('should create missing Stores', function () {
+      var context = new Context();
+
+      this.context.createStore('extra')
+        .define('key', 'value');
+      context.fromObject(JSON.parse(JSON.stringify(this.context)));
+
+      expect(context.getStore('extra')).to.exist();
+      expect('key' in context.getStore('extra')).to.be.true();
+      expect(context.getStore('extra').key).to.equal('value');
     });
   });
 });
